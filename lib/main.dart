aa9c7426c81d9a03933ec7074e25911eee666935
Pedro <pedrobraganick@hotmail.com>
2019-12-11 19:11:52 -0200

    commit envio de mensagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,200 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main(){
-  Firestore.instance.collection("teste").document("teste").setData({"teste" : "teste"});
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MyApp());
+}
+
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
+
+final googleSingIn = GoogleSignIn();
+final auth = FirebaseAuth.instance;
+
+Future _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSingIn.currentUser;
+  if (user == null) user = await googleSingIn.signInSilently();
+  if (user == null) user = await googleSingIn.signIn();
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials =
+        await googleSingIn.currentUser.authentication;
+    await auth.signInWithCredential(GoogleAuthProvider.getCredential(
+        idToken: credentials.idToken, accessToken: credentials.accessToken));
+  }
+}
+
+_handleSubmitted(String text) async{
+ await _ensureLoggedIn();
+ _sendMessage(text: text);
+}
+
+void _sendMessage({String text, String imgUrl}){
+  Firestore.instance.collection("messages").add({
+    "text" :text,
+    "imgUrl":imgUrl,
+    "senderName": googleSingIn.currentUser.displayName,
+    "senderPhotoUrl": googleSingIn.currentUser.photoUrl
+  });
 }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Chat App",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
   }
 }
 
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[ChatMessage(), ChatMessage(), ChatMessage()],
+              ),
+            ),
+            Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isComposing = false;
+final _textControlller = TextEditingController();
+
+void _reset() {
+  _textControlller.clear();
+  setState(() {
+    _isComposing = false;
+  });
+}
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[100])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                controller: _textControlller,
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: (text){ _handleSubmitted(text); _reset();},
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("Enviar"),
+                      onPressed: _isComposing ? (){_handleSubmitted(_textControlller.text); _reset();} : null,
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _isComposing ? () { _handleSubmitted(_textControlller.text); _reset();} : null,
+                    ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Daniel",
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("TESTE"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
